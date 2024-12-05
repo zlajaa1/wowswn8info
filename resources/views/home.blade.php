4a8c616cf0ab1d859926214364b1a72c66a9607b
zlajaa1 <vujicic.zlatko@gmail.com>
2024-12-05 14:09:50 +0100

    layout, header partials add, layout defined, home route defined

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+
+@section('title', 'Home Page')
+
+@section('content')
+  <h1>Welcome to home</h1>
+  <p>This is the home page.</p>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+  <p>&copy; {{ date('Y') }} wows. All rights reserved.</p>
+</footer>
