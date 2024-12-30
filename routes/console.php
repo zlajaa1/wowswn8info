e678e76eb76b9419dd98e6985883c8bf4819ad87
DannyCroDev <danielrajic26@outlook.com>
2024-12-30 11:23:44 +0100

    refactored the cron job structure, code change

--- routes/console.php
+++ routes/console.php
@@ -1,20 +1,20 @@
 <?php
 
+use App\Http\Controllers\PlayerShipController;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\ShipController;
 
-
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
 // Add a test task that runs every minute
-Schedule::call(function () {
-    Log::info('Test scheduler task running at: ' . now());
-})->everyMinute();
+//Schedule::call(function () {
+//  Log::info('Test scheduler task running at: ' . now());
+//})->everyMinute();
 
 Schedule::call(function () {
     try {
@@ -24,4 +24,15 @@ Schedule::call(function () {
     } catch (\Exception $e) {
         Log::error('Fetch ships cron failed from console.php: ' . $e->getMessage());
     }
-})->everyTwoMinutes();
+})->monthlyOn(15, '01:00');
+
+
+Schedule::call(function () {
+    try {
+        Log::info("Fetch player-ships cron started from console.php");
+        app(PlayerShipController::class)->updatePlayerShips();
+        Log::info("Fetch player-ships cron completed from console.php");
+    } catch (\Exception $e) {
+        Log::error('Fetch player-ships cron failed from console.php: ' . $e->getMessage());
+    }
+})->dailyAt('02:00');
